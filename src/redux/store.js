0e37203cc8886cb28dd5fe49c06dbae4e1a686bb
sksurendrakmr <sksurendrakmr@gmail.com>
2022-04-16 00:37:42 +0530

    Async actions config

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,12 @@ import { composeWithDevTools } from "@redux-devtools/extension";
 
 import cakeReducer from "./cake/cakeReducer";
 import { iceCreamReducer } from "./iceCream/iceCreamReducer";
+import { userReducer } from "./user/userReducer";
 
 const rootReducers = combineReducers({
   cake: cakeReducer,
   iceCream: iceCreamReducer,
+  user: userReducer,
 });
 
 //add logger middleware - logs information related to redux
