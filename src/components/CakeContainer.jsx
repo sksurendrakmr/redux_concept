ff41494809448c29b6734c71cbb7068410386262
sksurendrakmr <sksurendrakmr@gmail.com>
2022-04-15 19:03:02 +0530

    Connect redux state and dispatch to component props

--- src/components/CakeContainer.jsx
+++ src/components/CakeContainer.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
+import { buyCake } from "../redux";
 
-export const CakeContainer = () => {
+export const CakeContainer = (props) => {
   return (
     <div>
-      <h2>Number of cakes</h2>
-      <button>Buy cake</button>
+      <h2>Number of cakes - {props.numOfCakes}</h2>
+      <button onClick={() => props.buyCake()}>Buy cake</button>
     </div>
   );
 };
+
+//Get hold of redux state and how to dispatch an action from within react component
+const mapStateToProps = (state) => {
+  return {
+    numOfCakes: state.numOfCakes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
