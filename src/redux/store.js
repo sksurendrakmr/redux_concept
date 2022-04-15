26d41b926215a9510b6464d3def2a515ba7176c7
sksurendrakmr <sksurendrakmr@gmail.com>
2022-04-15 19:54:42 +0530

    Redux Devtool Extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import logger from "redux-logger";
+import { composeWithDevTools } from "@redux-devtools/extension";
+
 import cakeReducer from "./cake/cakeReducer";
 import { iceCreamReducer } from "./iceCream/iceCreamReducer";
 
@@ -9,6 +11,9 @@ const rootReducers = combineReducers({
 });
 
 //add logger middleware - logs information related to redux
-const store = createStore(rootReducers, applyMiddleware(logger)); //provide this store to react with Provider component
+const store = createStore(
+  rootReducers,
+  composeWithDevTools(applyMiddleware(logger))
+); //provide this store to react with Provider component
 
 export default store;
